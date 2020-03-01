3048f03e09f6cad4e005b5342ac4fb0cce3a5016
Daniel Garcia <pel_daniel@hotmail.com>
2020-02-29 21:25:34 -0800

    Add 'CodeBlock' and 'FunctionCallCollection' components

---
+++ src/CodeBlock.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './CodeBlock.css';
+
+export const CodeBlock = () => {
+  return (
+    <div className="code-block">
+      <a>plus</a>
+    </div>
+  );
+};
---
+++ src/FunctionCallCollection.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { CodeBlock } from './CodeBlock';
+
+import './FunctionCallCollection.css';
+
+export const FunctionCallCollection = () => {
+  return (
+    <div className="fcc">
+      <CodeBlock />
+    </div>
+  );
+};
