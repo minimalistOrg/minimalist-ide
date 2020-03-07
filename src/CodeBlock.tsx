c6d743a86fe4902dad7bd3a7a4469861ba98b47f
Daniel Garcia <pel_daniel@hotmail.com>
2020-03-07 15:33:10 -0800

    Add header, body & styles to CodeBlock

--- src/CodeBlock.tsx
+++ src/CodeBlock.tsx
@@ -3,9 +3,17 @@ import React from 'react';
 import './CodeBlock.css';
 
 export const CodeBlock = () => {
+  // Add function name/namespace
+
   return (
     <div className="code-block">
-      <a>plus</a>
+      <div className="code-block-header">
+        plus<span className="code-block-arguments">(a, b)</span>
+      </div>
+
+      <div className="code-block-body">
+        a <a>+</a> b
+      </div>
     </div>
   );
 };
