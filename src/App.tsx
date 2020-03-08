06d054f07183f1242e99d3df12d5d2c3a785fbcd
Daniel Garcia <pel_daniel@hotmail.com>
2020-03-08 16:46:24 -0700

    Use es6 function to define App component

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,10 @@ import { FunctionCallCollection } from './FunctionCallCollection';
 
 import './App.css';
 
-function App() {
+export const App = () => {
   return (
     <div className="App">
       <FunctionCallCollection />
     </div>
   );
-}
-
-export default App;
+};
