894361fbd23b111b5c832c1e3d150535ea679cb0
Daniel Garcia <pel_daniel@hotmail.com>
2020-04-15 18:33:30 -0500

    Move css code from ConstantBlock.css to ConstantAssignmentBlock.css

--- src/blockComponents/ConstantAssignmentBlock.tsx
+++ src/blockComponents/ConstantAssignmentBlock.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { TConstantAssignment, TAstToHtml } from "../types/CodeBlockTypes";
 
+import './ConstantAssignmentBlock.css';
+
 export const ConstantAssignmentBlock = (
   {
     ast: { id, name, value },
@@ -15,10 +17,14 @@ export const ConstantAssignmentBlock = (
 ) => {
   return (
     <div key={id} className="constant-assignment">
-      <div className="constant-assignment-name">{name}</div>
-      <div className="constant-assignment-equal-sign">=</div>
+      <div className="constant-assignment-start">
+        <div className="constant-assignment-name">{name}</div>
+        <div className="constant-assignment-equal-sign">=</div>
+      </div>
 
-      {astToHtml(value, index)}
+      <div className="constant-assignment-value">
+        {astToHtml(value, index)}
+      </div>
     </div>
   );
 }
