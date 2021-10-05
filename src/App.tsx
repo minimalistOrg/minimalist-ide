37f671ace62ac04d35d2d344fb86d966438c8bca
Daniel Garcia <pel_daniel@hotmail.com>
2021-10-05 23:14:48 +0100

    Extract fakeCallRows into a separate file

--- src/App.tsx
+++ src/App.tsx
@@ -1,106 +1,11 @@
 import React, { useState } from "react";
 import { FunctionCallRow } from "./FunctionCallRow";
+import { fakeCallRows } from "./fakeCallRows";
 
 import './App.css';
 
 export const App = () => {
-  const [editorState] = useState({
-    callRows: [
-      [
-        {
-          name: "gameLoop",
-          blockArguments: ["a"],
-          ast: [
-            {
-              type: "variable-assignment" as const,
-              id: "turn",
-              name: "turn",
-              value: {
-                type: "constant" as const,
-                value: 0
-              }
-            }
-          ]
-        }
-      ],
-      [
-        {
-          name: "conditional",
-          blockArguments: ["a"],
-          ast: [
-            {
-              type: "conditional" as const,
-              condition: {
-                type: "constant" as const,
-                value: false,
-              },
-              trueBlock: [
-                {
-                  type: "constant-assignment" as const,
-                  id: "x",
-                  name: "x",
-                  value: {
-                    type: "std-library" as const,
-                    id: 4,
-                    callArguments: [
-                      {
-                        type: "constant" as const,
-                        value: "a"
-                      },
-                      {
-                        type: "constant" as const,
-                        value: "a"
-                      }
-                    ],
-                    name: "plus",
-                    alias: []
-                  }
-                }
-              ],
-              falseBlock: [
-                {
-                  type: "constant" as const,
-                  value: 5
-                }
-              ],
-            }
-          ]
-        },
-      ],
-      [
-        {
-          name: "nested",
-          blockArguments: ["a"],
-          ast: [
-            {
-              id: 3,
-              callArguments: [
-                {
-                  id: 4,
-                  callArguments: [
-                    {
-                      type: "constant" as const,
-                      value: "a"
-                    },
-                    {
-                      type: "constant" as const,
-                      value: "a"
-                    }
-                  ],
-                  name: "plus",
-                  alias: [],
-                  type: "std-library" as const
-                }
-              ],
-              name: "plus",
-              alias: [],
-              type: "std-library" as const
-            }
-          ]
-        }
-      ]
-    ]
-  });
+  const [editorState] = useState(fakeCallRows);
 
   return (
     <div className="App">
