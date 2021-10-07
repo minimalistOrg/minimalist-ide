bdd2b5d8f02c4dca9737aceb51723cc2471a9a4b
Daniel Garcia <pel_daniel@hotmail.com>
2021-10-07 15:52:22 +0100

    Introduce libraryMock

--- src/FunctionCallRow.tsx
+++ src/FunctionCallRow.tsx
@@ -1,16 +1,23 @@
 import React from "react";
 import { CodeBlock } from "./CodeBlock";
-import { TCodeBlock } from "./types/CodeBlockTypes";
+import { libraryMock } from "./mocks/libraryMock";
 
 import './FunctionCallRow.css';
 
+type TmpFunctionId = keyof typeof libraryMock;
+
 export const FunctionCallRow = (
-  { functionRow }: { functionRow: TCodeBlock[] }
+  { functionRow }: { functionRow: string[] }
 ) => {
   return (
     <div className="fcr">
-      {functionRow.map((codeBlock, index) =>
-        <CodeBlock {...codeBlock} key={index} />
+      {functionRow.map((functionId, index) => {
+        const codeBlock = libraryMock[functionId as TmpFunctionId];
+
+        return (
+          <CodeBlock {...codeBlock} key={index} />
+        );
+      }
       )}
     </div>
   );
---
+++ src/mocks/libraryMock.ts
@@ -0,0 +1,159 @@
+export const libraryMock = {
+  "1": {
+    name: "gameLoop",
+    blockArguments: ["a"],
+    ast: [
+      {
+        type: "variable-assignment" as const,
+        id: "turn",
+        name: "turn",
+        value: {
+          type: "constant" as const,
+          value: 0
+        }
+      },
+      {
+        type: "variable-assignment" as const,
+        id: "board",
+        name: "board",
+        value: {
+          type: "list" as const,
+          value: [
+            {
+              type: "list" as const,
+              value: [
+                {
+                  type: "constant" as const,
+                  value: 0
+                },
+                {
+                  type: "constant" as const,
+                  value: 0
+                },
+                {
+                  type: "constant" as const,
+                  value: 0
+                }
+              ]
+            },
+            {
+              type: "list" as const,
+              value: [
+                {
+                  type: "constant" as const,
+                  value: 0
+                },
+                {
+                  type: "constant" as const,
+                  value: 0
+                },
+                {
+                  type: "constant" as const,
+                  value: 0
+                }
+              ]
+            },
+            {
+              type: "list" as const,
+              value: [
+                {
+                  type: "constant" as const,
+                  value: 0
+                },
+                {
+                  type: "constant" as const,
+                  value: 0
+                },
+                {
+                  type: "constant" as const,
+                  value: 0
+                }
+              ]
+            }
+          ]
+        }
+      },
+      {
+        type: "variable-assignment" as const,
+        id: "gameEnded?",
+        name: "gameEnded?",
+        value: {
+          type: "constant" as const,
+          value: false
+        }
+      }
+    ]
+  },
+  "2": {
+    name: "conditional",
+    blockArguments: ["a"],
+    ast: [
+      {
+        type: "conditional" as const,
+        condition: {
+          type: "constant" as const,
+          value: false,
+        },
+        trueBlock: [
+          {
+            type: "constant-assignment" as const,
+            id: "x",
+            name: "x",
+            value: {
+              type: "std-library" as const,
+              id: 101,
+              callArguments: [
+                {
+                  type: "constant" as const,
+                  value: "a"
+                },
+                {
+                  type: "constant" as const,
+                  value: "a"
+                }
+              ],
+              name: "plus",
+              alias: []
+            }
+          }
+        ],
+        falseBlock: [
+          {
+            type: "constant" as const,
+            value: 5
+          }
+        ],
+      }
+    ]
+  },
+  "3": {
+    name: "nested",
+    blockArguments: ["a"],
+    ast: [
+      {
+        id: 101,
+        callArguments: [
+          {
+            id: 101,
+            callArguments: [
+              {
+                type: "constant" as const,
+                value: "a"
+              },
+              {
+                type: "constant" as const,
+                value: "a"
+              }
+            ],
+            name: "plus",
+            alias: [],
+            type: "std-library" as const
+          }
+        ],
+        name: "plus",
+        alias: [],
+        type: "std-library" as const
+      }
+    ]
+  }
+};
